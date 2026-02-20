8c0619454a1344784bcd6686c18b03a4e753f75c
rafeieamirhosein <rafeieamirhosein@gmail.com>
2026-02-20 16:34:33 +0330

    fix rtl

--- src/HOC/MUI/MUIRtlConfig/index.tsx
+++ src/HOC/MUI/MUIRtlConfig/index.tsx
@@ -1,31 +1,17 @@
-import { ReactNode, useState } from 'react';
-import createCache, { EmotionCache } from '@emotion/cache';
-import { CacheProvider } from '@emotion/react';
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import rtlPlugin from "@mui/stylis-plugin-rtl";
+import { ReactNode } from "react";
 
-type EmotionCacheWithFlush = EmotionCache & {
-  inserted: Record<string, string | true>;
-};
+// Create rtl cache
+const rtlCache = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
-const createRtlCache = (): EmotionCacheWithFlush => {
-  const cache = createCache({
-    key: 'muirtl',
-  }) as EmotionCacheWithFlush;
-
-  cache.compat = true;
-
-  const prevInsert = cache.insert;
-
-  cache.insert = (...args) => {
-    return prevInsert(...args);
-  };
-
-  return cache;
-};
-
-const MUIRtlConfig = ({ children }: { children: ReactNode }) => {
-  const [cache] = useState(createRtlCache);
-
-  return <CacheProvider value={cache}>{children}</CacheProvider>;
-};
+function MUIRtlConfig({ children }: { children: ReactNode }) {
+  return <CacheProvider value={rtlCache}>{children}</CacheProvider>;
+}
 
 export default MUIRtlConfig;
