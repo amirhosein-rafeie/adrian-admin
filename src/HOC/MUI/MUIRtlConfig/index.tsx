14976c5117e08dca14214d3608984465d96788dd
Amirhosein Rafeie <103883902+amirhosein-rafeie@users.noreply.github.com>
2026-02-24 16:18:25 +0330

    Use MUI RTL stylis plugin to avoid unsafe selector warning

--- src/HOC/MUI/MUIRtlConfig/index.tsx
+++ src/HOC/MUI/MUIRtlConfig/index.tsx
@@ -1,7 +1,7 @@
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { prefixer } from "stylis";
-import rtlPlugin from "stylis-plugin-rtl";
+import rtlPlugin from "@mui/stylis-plugin-rtl";
 import { ReactNode } from "react";
 
 // Create rtl cache
